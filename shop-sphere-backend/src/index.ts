fa74b137e9c9866b4bd7da1d43fa14ecc8ae7fb2
Anukalp-dev <anukalp.tech@gmail.com>
2025-08-25 21:57:00 +0530

    server running for the first time

---
+++ shop-sphere-backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("hello World")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port http://localhost:${process.env.PORT || 3000}`);
+});
