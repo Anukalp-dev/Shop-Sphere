d70fda6a8951bc24b6833c7a7dbe9f3ed7e9a744
Anukalp-dev <anukalp.tech@gmail.com>
2025-08-30 21:31:23 +0530

    create cart model for mongoose

--- shop-sphere-backend/src/models/cart.model.ts
+++ shop-sphere-backend/src/models/cart.model.ts
@@ -1,19 +1,53 @@
-import mongoose, { Types, model, Schema } from "mongoose";
+import mongoose, { Types, model, Schema, mongo } from "mongoose";
 
 export interface ICart {
     user: Types.ObjectId;
-    items: [
-        {
+    items: {
             product: Types.ObjectId;
-            quantity: Number;
-            price: Number
-        }
-    ];
-    totalPrice: Number
+            quantity: number;
+            price: number
+        }[];
+    totalPrice: number
 }
 
 const cartSchema = new Schema<ICart>({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    items: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1
+        },
+        price: {
+            type: Number,
+            required: true
+        }
+
+    }],
+
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 
 })
 
-export const Cart = model<ICart>("Cart", cartSchema)
+cartSchema.pre("save", function(next){
+    this.totalPrice = this.items.reduce((acc, item) => acc + item.quantity * item.price, 0);
+    next()
+});
+
+export const Cart = model<ICart>("Cart", cartSchema);
